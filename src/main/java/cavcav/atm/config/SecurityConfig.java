f12cfb6d1cabb01db02675fcc775972dbfe9b23c
Ömer Sarıtoy <101520451+omersaritoy@users.noreply.github.com>
2025-06-30 22:15:28 +0300

    overview class

--- src/main/java/cavcav/atm/config/SecurityConfig.java
+++ src/main/java/cavcav/atm/config/SecurityConfig.java
@@ -10,7 +10,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.stereotype.Repository;
+
 
 @EnableWebSecurity
 @Configuration
